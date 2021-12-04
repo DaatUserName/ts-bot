1e5747a5e64c95b5e215bb95a51c5890c846e517
Toby <40370496+DaatUserName@users.noreply.github.com>
2021-12-04 13:53:39 +0100

    stuff

--- src/interfaces/Command.ts
+++ src/interfaces/Command.ts
@@ -2,7 +2,7 @@ import { Bot } from '../client/Client';
 import { Message } from 'discord.js';
 
 export interface RunFunc {
-	(client: Bot, message: Message, args: string[]): Promise<void>;
+	(client: Bot, message: Message, args: string[]): Promise<Message>;
 }
 
 export interface Command {
